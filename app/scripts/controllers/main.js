8f8fc5dbb5213f68948bd6e57803b5caa7b5d56f
Daniil Molodkov <yanko86@gmail.com>
2013-11-25 16:56:34 -0500

    Added tags Added preview

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,5 +7,30 @@ angular.module('photoshoplrNgApp.controllers', [])
 	    	$scope.title = blogPosts.blog.title;
 	    	$scope.description = blogPosts.blog.description;
 	    	$scope.posts = blogPosts.posts;
+	    	$scope.postCount = blogPosts.blog.posts;
+
+	    	// Set initial preview
+	    	$scope.showDetails($scope.posts[0]);
+
+	    	updatePaginator();
 	    });
+
+	    $scope.showDetails = function(post) {
+	    	$scope.postBody = post.body;
+	    }
+
+	    $scope.goToPage = function(pageN) {
+	    	$scope.currPage = pageN;
+	    }
+
+	    function updatePaginator() {
+	    	var pageCount = Math.ceil($scope.postCount / 20);
+	    	
+	    	$scope.pages = [];
+	    	$scope.currPage = 0;
+
+	    	for (var i = 0; i < pageCount; i++) {
+	    		$scope.pages.push(i);
+	    	}
+	    }
   }]);
