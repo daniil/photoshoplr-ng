e174fdb6aa12ee7ab1dce4235a4ef5b9b7729248
Daniil Molodkov <mr.me@daniil.ca>
2013-11-25 21:17:41 -0500

    Fixed spacing issues Added timestamps

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,41 +1,43 @@
 'use strict';
 
 angular.module('photoshoplrNgApp.controllers', [])
-  .controller('MainCtrl', ['$scope', '$resource', 'TumblrAPI', 'Settings', 
-  	function ($scope, $resource, TumblrAPI, Settings) {
-	    TumblrAPI.blogPosts({offset: 0}, function(blogPosts) {
-	    	$scope.title = blogPosts.blog.title;
-	    	$scope.description = blogPosts.blog.description;
-	    	$scope.posts = blogPosts.posts;
-	    	$scope.postCount = blogPosts.blog.posts;
+  .controller('MainCtrl', ['$scope', '$resource', 'TumblrAPI', 'Settings',
+    function ($scope, $resource, TumblrAPI, Settings) {
+      TumblrAPI.blogPosts({offset: 0}, function(blogPosts) {
+        $scope.title = blogPosts.blog.title;
+        $scope.description = blogPosts.blog.description;
+        $scope.posts = blogPosts.posts;
+        $scope.postCount = blogPosts.blog.posts;
 
-	    	// Set initial preview
-	    	$scope.showDetails($scope.posts[0]);
+        $scope.showDetails($scope.posts[0]);
 
-	    	populatePaginator();
-	    });
+        populatePaginator();
+      });
 
-	    $scope.showDetails = function(post) {
-	    	$scope.selectedPost = post;
-	    }
+      $scope.showDetails = function(post) {
+        $scope.selectedPost = post;
+      };
 
-	    $scope.goToPage = function(pageN) {
-	    	TumblrAPI.blogPosts({offset: pageN * Settings.postLimit}, function(blogPosts) {
-	    		$scope.posts = blogPosts.posts;
-	    		$scope.showDetails($scope.posts[0]);
+      $scope.goToPage = function(pageN) {
+        TumblrAPI.blogPosts({offset: pageN * Settings.postLimit}, function(blogPosts) {
+          $scope.posts = blogPosts.posts;
+          $scope.showDetails($scope.posts[0]);
 
-	    		$scope.currPage = pageN;
-	    	});
-	    }
+          $scope.currPage = pageN;
 
-	    function populatePaginator() {
-	    	var pageCount = Math.ceil($scope.postCount / Settings.postLimit);
-	    	
-	    	$scope.pages = [];
-	    	$scope.currPage = 0;
+          window.scrollTo(0, 0);
+        });
+      };
 
-	    	for (var i = 0; i < pageCount; i++) {
-	    		$scope.pages.push(i);
-	    	}
-	    }
-  }]);
+      function populatePaginator() {
+        var pageCount = Math.ceil($scope.postCount / Settings.postLimit);
+        
+        $scope.pages = [];
+        $scope.currPage = 0;
+
+        for (var i = 0; i < pageCount; i++) {
+          $scope.pages.push(i);
+        }
+      }
+    }
+  ]);
--- app/scripts/services/main.js
+++ app/scripts/services/main.js
@@ -1,30 +1,30 @@
 'use strict';
 
 angular.module('photoshoplrNgApp.services', [])
-	.factory('Settings', [
-		function() {
-			return {
-				postLimit: 10
-			}
-		}
-	])
-	.factory('TumblrAPI', ['$resource', 'Settings', 
-		function($resource, Settings) {
-			var apiURL = 'http://api.tumblr.com/v2/blog/madebyvadim.tumblr.com/';
+  .factory('Settings', [
+    function() {
+      return {
+        postLimit: 10
+      };
+    }
+  ])
+  .factory('TumblrAPI', ['$resource', 'Settings',
+    function($resource, Settings) {
+      var apiURL = 'http://api.tumblr.com/v2/blog/madebyvadim.tumblr.com/';
 
-			return $resource(apiURL + 'posts', {
-				api_key: '5qpBPu3mITiBI7oB9GwK5bBCYlMmt2Y43FD5RuXzJu3OWWPIBr',
-				callback: 'JSON_CALLBACK',
-				limit: Settings.postLimit,
-				offset: '@id'
-			}, 
-			{ 
-				blogPosts: { 
-					method: 'JSONP',
-					transformResponse: function(data) {
-						return data.response;
-					}
-				}
-			});
-		}
-	]);
+      return $resource(apiURL + 'posts', {
+        api_key: '5qpBPu3mITiBI7oB9GwK5bBCYlMmt2Y43FD5RuXzJu3OWWPIBr',
+        callback: 'JSON_CALLBACK',
+        limit: Settings.postLimit,
+        offset: '@id'
+      },
+      {
+        blogPosts: {
+          method: 'JSONP',
+          transformResponse: function(data) {
+            return data.response;
+          }
+        }
+      });
+    }
+  ]);
