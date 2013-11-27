f50b1a0b23460216226edbd3240ff9d599756879
Daniil Molodkov <yanko86@gmail.com>
2013-11-27 16:30:01 -0500

    Added search element

--- app/scripts/filters/main.js
+++ app/scripts/filters/main.js
@@ -4,8 +4,7 @@ angular.module('photoshoplrNgApp.filters', [])
   .filter('hasTags', function() {
     var hasTagsFilter = function(posts, settings) {
       var filteredPosts = _.filter(posts, function(post) { 
-        var matchCount = 0,
-            isMatch;
+        var matchCount = 0;
 
         _.each(post.tags, function(val) {
           if (_.contains(settings.filter, val)) {
